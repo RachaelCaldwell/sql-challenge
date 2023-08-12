63d0350aa78515d8501a7a048f071096e853e7cf
Rachael Caldwell <rachael.piper@cms.k12.nc.us>
2023-08-12 16:08:04 -0400

    cleaned schema code

--- EmployeeSQL/employee_schema.sql
+++ EmployeeSQL/employee_schema.sql
@@ -3,13 +3,11 @@ CREATE TABLE departments(
 	dept_no VARCHAR(5) PRIMARY KEY NOT NULL,
 	dept_name VARCHAR(30) NOT NULL
 );
--- SELECT * FROM departments LIMIT 5
 
 CREATE TABLE titles(
   title_id VARCHAR(5) PRIMARY KEY NOT NULL,
   title VARCHAR(30) NOT NULL
  );
--- SELECT * FROM titles LIMIT 5
 
 CREATE TABLE employees(
   emp_no INTEGER PRIMARY KEY NOT NULL,
@@ -20,25 +18,21 @@ CREATE TABLE employees(
   sex VARCHAR NOT NULL,
   hire_date DATE NOT NULL
 ); 
--- SELECT * FROM employees LIMIT 5
 
 CREATE TABLE dept_emp(
   emp_no INTEGER REFERENCES employees(emp_no) NOT NULL,
   dept_no VARCHAR(5) REFERENCES departments(dept_no) NOT NULL,
   PRIMARY KEY(emp_no, dept_no)
 );
--- SELECT * FROM dept_emp LIMIT 5
 
 CREATE TABLE dept_manager(
   dept_no VARCHAR(5) REFERENCES departments(dept_no) NOT NULL,
   emp_no INTEGER REFERENCES employees(emp_no) NOT NULL,
   PRIMARY KEY (dept_no, emp_no)
 );
--- SELECT * FROM dept_manager LIMIT 5
 
 CREATE TABLE salaries(
   emp_no INTEGER REFERENCES employees(emp_no) NOT NULL,
   salary INTEGER NOT NULL,
   PRIMARY KEY (emp_no)
 );
--- SELECT * FROM salaries LIMIT 5
